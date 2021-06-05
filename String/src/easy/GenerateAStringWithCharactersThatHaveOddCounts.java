19229493398632fb681c167ffc4877fd62a2c807
PravinMukesh <mukesh.pravin@gmail.com>
2021-06-05 20:47:21 +0530

    Easy Problems in String - leetcode

---
+++ String/src/easy/GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class GenerateAStringWithCharactersThatHaveOddCounts {
+
+	public static String generateTheString(int n) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++)
+		{
+			
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		int n = 2;
+		System.out.println(generateTheString(n));
+	}
+
+}
