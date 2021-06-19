e6c2b49bd6ceef01fd4675d58704dc2285b40a93
PravinMukesh <mukesh.pravin@gmail.com>
2021-06-19 09:10:56 +0530

    Medium Problems in String - leetcode

--- String/src/medium/EvaluateTheBracketPairsOfAString.java
+++ String/src/medium/EvaluateTheBracketPairsOfAString.java
@@ -1,18 +1,22 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EvaluateTheBracketPairsOfAString {
 
 	public static String evaluate(String s, List<List<String>> knowledge) {
 		StringBuffer sb = new StringBuffer();
+		Map<String, String> map = new HashMap<>(knowledge.size());
+		for (List<String> list : knowledge) {
+			map.put(list.get(0), list.get(1));
+		}
 		int start = 0;
 		int end = 0;
-		boolean flag = true;
 		sb.append(s);
 		for (int i = 0; i < sb.length(); i++) {
-			flag = true;
 			if (sb.charAt(i) == '(') {
 				start = i;
 				for (int j = i + 1; j < sb.length(); j++) {
@@ -21,55 +25,49 @@ public class EvaluateTheBracketPairsOfAString {
 						break;
 					}
 				}
-				for (int k = 0; k < knowledge.size(); k++) {
-					if (knowledge.get(k).get(0).toString().equalsIgnoreCase(sb.substring(start + 1, end))) {
-						sb.replace(start, end + 1, knowledge.get(k).get(1));
-						flag = false;
-						break;
-					}
-				}
-				if (flag) {
+				if (map.containsKey(sb.substring(start + 1, end))) {
+					sb.replace(start, end + 1, map.get(sb.substring(start + 1, end)));
+				} else {
 					sb.replace(start, end + 1, "?");
 				}
 			}
-
 		}
 		return sb.toString();
 	}
 
 	public static void main(String[] args) {
-//		String s = "(name)is(age)yearsold";
+		String s = "(name)is(age)yearsold";
 //		String s = "(a)(a)(a)aaa";
 //		String s = "(a)(b)";
 //		String s = "hi(name)";
-		String s = "(zib)";
+//		String s = "(zib)";
 
 		List<String> list1 = new ArrayList<>();
-		list1.add("qfviuwjo");
-		list1.add("w");
+		list1.add("name");
+		list1.add("bob");
 
 		List<String> list2 = new ArrayList<>();
-		list2.add("kppdajmk");
-		list2.add("n");
-		
-		List<String> list3 = new ArrayList<>();
-		list3.add("zib");
-		list3.add("c");
-		
-		List<String> list4 = new ArrayList<>();
-		list4.add("ztfljcff");
-		list4.add("i");
-		
-		List<String> list5 = new ArrayList<>();
-		list5.add("aoayyihi");
-		list5.add("d");
+		list2.add("age");
+		list2.add("two");
+
+//		List<String> list3 = new ArrayList<>();
+//		list3.add("zib");
+//		list3.add("c");
+//
+//		List<String> list4 = new ArrayList<>();
+//		list4.add("ztfljcff");
+//		list4.add("i");
+//
+//		List<String> list5 = new ArrayList<>();
+//		list5.add("aoayyihi");
+//		list5.add("d");
 
 		List<List<String>> knowledge = new ArrayList<List<String>>();
 		knowledge.add(list1);
 		knowledge.add(list2);
-		knowledge.add(list3);
-		knowledge.add(list4);
-		knowledge.add(list5);
+//		knowledge.add(list3);
+//		knowledge.add(list4);
+//		knowledge.add(list5);
 		System.out.println(evaluate(s, knowledge));
 	}
 
