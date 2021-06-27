338fcde90f9b710ca0fe9959111a88b435cbd3fd
PravinMukesh <mukesh.pravin@gmail.com>
2021-06-27 11:20:58 +0530

    Easy Problems in String - leetcode

--- String/src/easy/DeleteColumnsToMakeSorted.java
+++ String/src/easy/DeleteColumnsToMakeSorted.java
@@ -5,22 +5,28 @@ public class DeleteColumnsToMakeSorted {
 	public static int minDeletionSize(String[] strs) {
 		int deletedColumn = 0;
 		char prevChar = '\0';
-		for(int i = 0; i < strs[0].length(); i++) {
+		int i = 0;
+		int j = 0;
+		while (i < strs[0].length()) {
 			prevChar = '\0';
-			for(int j = 0; j < strs.length;j++) {
-				if(prevChar == '\0') {
+			j = 0;
+			while (j < strs.length) {
+				if (prevChar == '\0') {
 					prevChar = strs[j].charAt(i);
 				} else {
-					if(prevChar < strs[j].charAt(i)) {
+					if (prevChar < strs[j].charAt(i)) {
 						prevChar = strs[j].charAt(i);
-					} else if(prevChar > strs[j].charAt(i)) {
+						j++;
+					} else if (prevChar > strs[j].charAt(i)) {
 						deletedColumn++;
 						break;
 					} else {
 						prevChar = strs[j].charAt(i);
+						j++;
 					}
 				}
 			}
+			i++;
 		}
 		return deletedColumn;
 	}
@@ -28,10 +34,10 @@ public class DeleteColumnsToMakeSorted {
 	public static void main(String[] args) {
 //		String[] strs = {"abc", "bce", "cae"};
 //		String[] strs = { "cba", "daf", "ghi" };
-		String[] strs = { "a", "a", "c", "c" };
+//		String[] strs = { "a", "a", "q", "c" };
 //		String[] strs = {"ab","ab"};
-//		String[] strs = {"zyx","wvu","tsr"};
-//		String[] strs = {"rrjka","furtb","guzma"};
+		String[] strs = { "zyx", "wvu", "tsr" };
+//		String[] strs = {"rrjk","furt","guzm"};
 		System.out.println(minDeletionSize(strs));
 	}
 
