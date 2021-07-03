0dc0ddfe9159b516129720a0dd74c0bfce1054ba
PravinMukesh <mukesh.pravin@gmail.com>
2021-07-03 17:55:08 +0530

    Easy Problems in String - leetcode

--- String/src/easy/CrawlerLogFolder.java
+++ String/src/easy/CrawlerLogFolder.java
@@ -5,36 +5,25 @@ import java.util.Stack;
 public class CrawlerLogFolder {
 
 	public static int minOperations(String[] logs) {
-		Stack<String> stack = new Stack<>();
-		int log = 0;
-		while (log < logs.length) {
-			if (Character.isAlphabetic(logs[log].charAt(0))) {
-				stack.push(logs[log]);
-				log++;
-			} else if (Character.isDigit(logs[log].charAt(0))) {
-				stack.push(logs[log]);
-				log++;
-			} else {
-				if (logs[log].length() == 2)
-					log++;
-				else {
-					if (!stack.isEmpty()) {
-						stack.pop();
-						log++;
-					} else
-						log++;
-				}
-			}
+		int steps = 0;
+		for (String log : logs) {
+			if (log.equals("../")) {
+				if (steps > 0)
+					steps--;
+			} else if (log.equals("./"))
+				continue;
+			else
+				steps++;
 		}
-		return stack.size();
+		return steps;
 	}
 
 	public static void main(String[] args) {
 //		String[] logs = { "d1/", "d2/", "../", "d21/", "./" };
-//		String[] logs = {"d1/","d2/","./","d3/","../","d31/"};
-//		String[] logs = {"d1/","../","../","../"};
+//		String[] logs = { "d1/", "d2/", "./", "d3/", "../", "d31/" };
+		String[] logs = { "d1/", "../", "../", "../" };
 //		String[] logs = { "1/" };
-		String[] logs = { "./", "b9/", "aw8/", "./", "vw7/", "./", "d0/", "../", "p6/", "../" };
+//		String[] logs = { "./", "b9/", "aw8/", "./", "vw7/", "./", "d0/", "../", "p6/", "../" };
 		System.out.println(minOperations(logs));
 	}
 
