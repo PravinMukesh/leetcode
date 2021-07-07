480882e548ba6da4808d5b6e510c621e40cfca19
PravinMukesh <mukesh.pravin@gmail.com>
2021-07-07 19:05:54 +0530

    Easy Problems in String - leetcode

--- String/src/easy/ExcelSheetColumnTitle.java
+++ String/src/easy/ExcelSheetColumnTitle.java
@@ -4,29 +4,19 @@ public class ExcelSheetColumnTitle {
 
 	public static String convertToTitle(int columnNumber) {
 		StringBuilder sb = new StringBuilder();
-		int reminder = -1;
-		int value = 0;
-		while (columnNumber > 26) {
-			reminder = columnNumber % 26;
-			sb.append((char) (reminder - 1 + 'A'));
-			value = columnNumber - reminder;
-			columnNumber = value / 26;
+		while (columnNumber > 0) {
+			columnNumber--;
+			sb.insert(0, (char) ('A' + columnNumber % 26));
+			columnNumber /= 26;
 		}
-		if (columnNumber != 0) {
-			reminder = columnNumber % 26;
-			if (reminder != 0)
-				sb.append((char) (reminder - 1 + 'A'));
-			else
-				sb.append((char) (columnNumber - 1 + 'A'));
-		}
-		return sb.reverse().toString();
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
 //		int columnNumber = 1;
 //		int columnNumber = 28;
-//		int columnNumber = 701;
-		int columnNumber = 26;
+		int columnNumber = 701;
+//		int columnNumber = 26;
 //		int columnNumber = 2147483647;
 		System.out.println(convertToTitle(columnNumber));
 	}
