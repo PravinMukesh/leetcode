7d119c4f490f9eab236eab5e0dbd720dfad2435b
PravinMukesh <mukesh.pravin@gmail.com>
2021-09-30 21:07:54 +0530

    Easy problems in Arrays - leetcode

--- Arrays/src/easy/FinalValueOfVariableAfterPerformingOperations.java
+++ Arrays/src/easy/FinalValueOfVariableAfterPerformingOperations.java
@@ -2,8 +2,20 @@ package easy;
 
 public class FinalValueOfVariableAfterPerformingOperations {
 
-	public static void main(String[] args) {
+	public static int finalValueAfterOperations(String[] operations) {
+		int counter = 0;
+		for (String singleOperation : operations) {
+			if (singleOperation.contains("+")) {
+				counter++;
+			} else
+				counter--;
+		}
+		return counter;
+	}
 
+	public static void main(String[] args) {
+		String[] operations = { "++x", "x++", "x--" };
+		System.out.println(finalValueAfterOperations(operations));
 	}
 
 }
